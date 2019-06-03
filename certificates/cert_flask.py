a4025a1a820a2ea0305e7c51068ec9831abd35f8
Kenji Saito <ks91@sfc.wide.ad.jp>
2019-06-03 18:51:47 +0900

    structured so that it is easier now to run multiple applications on a server.

--- certificates/cert_flask.py
+++ certificates/cert_flask.py
@@ -26,19 +26,16 @@ import xml.etree.ElementTree as ET
 
 from bbc1.core.ethereum import bbc_ethereum
 from brownie import *
-from flask import Flask, render_template, request, redirect, url_for
+from flask import render_template, request, redirect, url_for
 
 
 S_CONTRACT_ADDRESS = '0xd123Ec03ACdbC36e4fA818c983C259049EE705e0'
 S_NETWORK = 'ropsten'
 
 
-app = Flask(__name__)
+def failure_template(dir, reason, root=''):
 
-
-def failure_template(reason, root=''):
-
-    return render_template('failure.html',
+    return render_template(os.path.join(dir, 'failure.html'),
             title='Certificate Verification - Failure',
             network=S_NETWORK, contract=S_CONTRACT_ADDRESS,
             reason=reason, root=root,
@@ -56,20 +53,19 @@ def get_date_string(timestamp):
     return s[0]
 
 
-@app.route('/')
-def index():
+def index(dir):
 
     cert_xml = request.args.get('certificate')
     subtree_string = request.args.get('subtree')
 
     if cert_xml is None or subtree_string is None:
-        return failure_template('no-query')
+        return failure_template(dir, 'no-query')
 
     try:
         root = ET.fromstring(cert_xml)
 
     except ET.ParseError:
-        return failure_template('xml-syntax')
+        return failure_template(dir, 'xml-syntax')
 
     dat = bytearray()
     for e in root:
@@ -91,7 +87,7 @@ def index():
     for node in nodes:
         s = node.split('-')
         if len(s) != 2 or not all(c in string.hexdigits for c in s[1]):
-            return failure_template('subtree-syntax', root=root)
+            return failure_template(dir, 'subtree-syntax', root=root)
         dic = {}
         dic['position'] = 'right' if s[0] == 'r' else 'left'
         dic['digest'] = s[1]
@@ -111,22 +107,18 @@ def index():
     block_no, digest0 = eth.verify_and_get_root(digest, subtree)
 
     if block_no <= 0:
-        return failure_template('digest-mismatch', root=root)
+        return failure_template(dir, 'digest-mismatch', root=root)
 
     block = network.web3.eth.getBlock(block_no)
 
     realtime = datetime.datetime.fromtimestamp(block['timestamp'])
 
-    return render_template('success.html',
+    return render_template(os.path.join(dir, 'success.html'),
             title='Certificate Vefirication - Success',
             root=root, network=S_NETWORK, contract=S_CONTRACT_ADDRESS,
             block_no=block_no, realtime=realtime,
             get_date_string=get_date_string,
             merkle_root=binascii.b2a_hex(digest0).decode())
-
-
-if __name__ == '__main__':
-    app.run()
 
 
 # end of cert_flask.py
